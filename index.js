69f92fe97d488617638b75b1badaf2699d4d5fe4
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-05-25 13:40:31 +0800

    Format event data to JSON

--- index.js
+++ index.js
@@ -7,14 +7,16 @@ const contactformapi = process.env.CONTACTFORMAPI;
 
 exports.handler = (event, context, callback) => {
   // Validate the recaptcha
-  const input_data = event;
+  console.log('event data is: ', event);
+  var stringify = JSON.stringify(event)
+  const input_data = JSON.parse(stringify);
   console.log('input_data is: ', input_data);
   console.log('Captcha Response is: ', input_data['g-recaptcha-response']);
   const postData = querystring.stringify({
     'secret': secretKey,
     'response': input_data['g-recaptcha-response']
   });
- 
+
   console.log('postData is: ', postData);
   const options = {
     hostname: 'www.google.com',
@@ -48,6 +50,14 @@ exports.handler = (event, context, callback) => {
           // NOTE: change this setting in production
           mode: 'no-cors',
           body: JSON.stringify(body)
+        });
+        callback(null, {
+          statusCode: '200',
+          headers: {
+            "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+            "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
+          },
+          body: JSON.stringify({ message: 'reCAPTCHA passed' })
         });
       } else {
         callback(null, {
