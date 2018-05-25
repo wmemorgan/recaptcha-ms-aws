05cfa0475b018f2b1e98f1d3f1bdf308a17cb474
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-05-25 12:12:50 +0800

    Remove console logs

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const request = require('request');
 const querystring = require('querystring');
 const AWS = require("aws-sdk");
 const secretKey = process.env.SECRET;
-const contactformapi = process.env.CONTACTFORMAPI
+const contactformapi = process.env.CONTACTFORMAPI;
 
 exports.handler = (event, context, callback) => {
   // Validate the recaptcha
   const input_data = event;
+  console.log('input_data is: ', input_data);
   console.log('Captcha Response is: ', input_data['g-recaptcha-response']);
   const postData = querystring.stringify({
     'secret': secretKey,
     'response': input_data['g-recaptcha-response']
   });
-  console.log(secretKey)
-  console.log('event is: ', event)
-  console.log('postData is: ', postData)
+ 
+  console.log('postData is: ', postData);
   const options = {
     hostname: 'www.google.com',
     port: 443,
@@ -36,43 +36,18 @@ exports.handler = (event, context, callback) => {
     message: input_data['message']
   }
 
-  console.log('Retrieved body content is:', body)
-
-  const submitContactForm = new request(contactformapi, {
-    method: 'POST',
-    // NOTE: change this setting in production
-    mode: 'no-cors',
-    body: JSON.stringify(body)
-  })
-
   const req = https.request(options, (res) => {
     res.setEncoding('utf8');
     res.on('data', (chunk) => {
       const captchaResponse = JSON.parse(chunk);
       console.log('captchaResponse is: ', captchaResponse)
       if (captchaResponse.success) {
-        let sns = new AWS.SNS();
-        delete input_data['g-recaptcha-response'];
-        let message = "";
-        Object.keys(input_data).forEach((key) => {
-          message += key + ':\n';
-          message += '\t' + input_data[key] + '\n\n';
-          // console.log(message);
-        });
-        const params = {
-          Message: message,
-          Subject: "${AWS::StackName} Contact Us",
-          TopicArn: "${ContactUsSNSTopic}"
-        };
-        sns.publish(params, (err, response) => {
-          callback(null, {
-            statusCode: '200',
-            headers: {
-              "Access-Control-Allow-Origin": "*", // Required for CORS support to work
-              "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
-            },
-            body: JSON.stringify(response)
-          });
+        console.log('Success! Sending the following contents to the contact form:', body)
+        const submitContactForm = new request(contactformapi, {
+          method: 'POST',
+          // NOTE: change this setting in production
+          mode: 'no-cors',
+          body: JSON.stringify(body)
         });
       } else {
         callback(null, {
